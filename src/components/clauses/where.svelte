3766752d1c609ce9582441059052583d3036a922
Johan JANIN <johan.janin@gmail.com>
2025-11-25 12:33:05 +0100

    feat: select tables columns from where clause

--- src/components/clauses/where.svelte
+++ src/components/clauses/where.svelte
@@ -3,9 +3,24 @@
   import type { Collection } from 'dexie';
   import type { Component } from 'svelte';
 
-  export class WhereBuilder extends Buildable<WhereCard, null> {
+  export class WhereBuilder extends Buildable<
+    WhereCard,
+    {
+      value: string | string[];
+    }
+  > {
     COMPONENT = import('./where.svelte').then((c) => c.default as Component);
-    binded = null;
+    binded = {
+      value: '',
+      table: '',
+      column: ''
+    };
+    constructor(card: WhereCard) {
+      super(card);
+      this.binded.value = (
+        card.data.type === 'in' || card.data.type === 'between' ? [] : ''
+      ) as typeof this.binded.value;
+    }
     build(query: Collection): Collection {
       // todo
       return query;
@@ -14,9 +29,28 @@
 </script>
 
 <script lang="ts">
+  import database from '$lib/database/main';
+
   let {
     builder
   }: {
     builder: WhereBuilder;
   } = $props();
+
+  let value = $state(builder.binded.value);
+  let table = $state('');
+  let column = $state('');
+  $effect(() => {
+    builder.binded = { value, table, column };
+  });
 </script>
+
+<select>
+  {#each database.tables as table}
+    <optgroup label={table.name}>
+      {#each table.schema.indexes as idx}
+        <option value={idx.name}>{idx.name}</option>
+      {/each}
+    </optgroup>
+  {/each}
+</select>
