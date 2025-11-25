57a597fadbea53465a1e8faae256ac2ee6f23d11
Johan JANIN <johan.janin@gmail.com>
2025-11-25 15:48:24 +0100

    feat: update table & column on where clause

--- src/components/clauses/where.svelte
+++ src/components/clauses/where.svelte
@@ -3,23 +3,37 @@
   import type { Collection } from 'dexie';
   import type { Component } from 'svelte';
 
-  export class WhereBuilder extends Buildable<
-    WhereCard,
-    {
-      value: string | string[];
-    }
-  > {
+  interface WhereBinds {
+    table: string;
+    column: string;
+    where:
+      | {
+          type: 'like' | 'gt' | 'eq' | 'lt';
+          value: string;
+        }
+      | {
+          type: 'between' | 'in';
+          values: [];
+        };
+  }
+
+  export class WhereBuilder extends Buildable<WhereCard, WhereBinds> {
     COMPONENT = import('./where.svelte').then((c) => c.default as Component);
     binded = {
-      value: '',
       table: '',
-      column: ''
-    };
+      column: '',
+      where: {
+        type: 'eq',
+        value: ''
+      }
+    } satisfies WhereBinds;
     constructor(card: WhereCard) {
       super(card);
-      this.binded.value = (
-        card.data.type === 'in' || card.data.type === 'between' ? [] : ''
-      ) as typeof this.binded.value;
+      this.binded.where = (
+        card.data.type === 'in' || card.data.type === 'between'
+          ? { type: card.data.type, values: [] }
+          : { type: 'eq', value: '' }
+      ) as typeof this.binded.where;
     }
     build(query: Collection): Collection {
       // todo
@@ -37,21 +51,27 @@
     builder: WhereBuilder;
   } = $props();
 
-  let value = $state(builder.binded.value);
+  let where = $state(builder.binded.where);
   let table = $state('');
   let column = $state('');
   $effect(() => {
-    builder.binded = { value, table, column };
+    builder.binded = { where, table, column };
+
+    console.log(builder.binded);
   });
 
   const whereType = builder.QRData.data.type;
 </script>
 
-<select>
+<select
+  onchange={(e) => {
+    [table, column] = e.currentTarget.value.split('.').map((v) => v.trim());
+  }}
+>
   {#each database.tables as table}
     <optgroup label={table.name}>
       {#each table.schema.indexes.filter((i) => !i.name.includes('id')) as idx}
-        <option value={idx.name}>{idx.name}</option>
+        <option value="{table.name}.{idx.name}">{idx.name}</option>
       {/each}
     </optgroup>
   {/each}
